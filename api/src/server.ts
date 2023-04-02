4b9ba6d857f273803ef40658677bc3b327486f9d
dfurtado <dfurtado.ext@acsp.com.br>
2023-04-02 14:10:49 -0300

    chore: criando layer para tratar a requisição

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,23 +1,13 @@
 import PgPromiseConnection from "./infra/database/PgPromiseConnection";
 import ExpressAdapter from "./infra/http/ExpressAdapter";
 import dotenv from 'dotenv';
+import AuthController from "./infra/controller/AuthController";
 
 dotenv.config();
 
 const connection = new PgPromiseConnection();
 
 const http = new ExpressAdapter();
-
-http.route("post", "/login", async function (params: any, body: any) {
-  return {
-    access_token: "123456"
-  };
-});
-
-http.route("get", "/", async function (params: any, body: any) {
-  return {
-    message: "Vou almoçar!"
-  };
-});
+new AuthController(http);
 
 http.listen(3000);
